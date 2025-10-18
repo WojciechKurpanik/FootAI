fab9072b98bd2e965355a9e268557e007469f435
WojciechKurpanik <wk306057@student.polsl.pl>
2025-10-18 20:25:03 +0200

    pretrained YOLOv11n model

---
+++ scripts/test_model.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+model = YOLO("models/FootAI_yolo11/weights/best.pt")
+test_dir = "../data/test/images/"
+out_dir = "../outputs/detections/"
+os.makedirs(out_dir, exist_ok=True)
+
+for img_name in os.listdir(test_dir):
+    if not img_name.endswith((".jpg", ".png")): continue
+    img_path = os.path.join(test_dir, img_name)
+    results = model.predict(source=img_path, imgsz=960, conf=0.25)
+    res_plotted = results[0].plot()
+    cv2.imwrite(os.path.join(out_dir, img_name), res_plotted)
+
+print("Detection saved in", out_dir)
