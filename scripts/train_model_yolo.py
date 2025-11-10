8668737948bc6155d4a57f7b11b7a7251e5b9a2b
WojciechKurpanik <wk306057@student.polsl.pl>
2025-11-10 15:42:54 +0100

    Commit without models! pretrain, validate

--- scripts/train_model_yolo.py
+++ scripts/train_model_yolo.py
@@ -1,17 +1,27 @@
 from ultralytics import YOLO
 
 def train():
-    model = YOLO("yolo11l.pt")
-
+    model = YOLO("runs/detect/yolo11x/weights/last.pt")
     model.train(
         data="../data/data.yaml",
-        epochs=150,
+        epochs=200,
         imgsz=960,
-        batch=8,
-        device="mps",
-        workers=0,
-        patience=50,
-        name="FootAI_yolo11l",
-        project="models",
-        pretrained=True
+        batch=6,
+        # device="mps",  # apple silicon
+        device=0, #windows z gpu
+        lr0=0.0008,
+        lrf=0.01,
+        optimizer="AdamW",
+        amp=True,
+        warmup_epochs=3,
+        freeze=0,
+        mosaic=1.0,
+        mixup=0.1,
+        copy_paste=0.3,
+        patience=75,
+        plots=True,
+        save_period=25,
+        cls=2.0,
+        name="FootAI_yolo11x",
+        project="models"
     )
